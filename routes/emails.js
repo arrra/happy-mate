134eb1c9536e81c535dcbe5b91aa0093846e4aab
Sagar Singh <ssing128@gmail.com>
2017-08-25 01:00:56 -0700

    basic test case

--- routes/emails.js
+++ routes/emails.js
@@ -4,4 +4,8 @@ const Router = require('express').Router;
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.status(200);
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -10,3 +10,4 @@ app.listen(3000, () => {
   console.log('listening on port 3000');
 })
 
+module.exports = app;
