4b9ea79f899dd5cfa55d5c3677db3c00cbd5bde3
Sagar Singh <ssing128@gmail.com>
2017-08-31 22:45:50 -0700

    update model

--- models/Message.js
+++ models/Message.js
@@ -3,12 +3,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const MessageSchema = new Schema({
+const ConversationSchema = new Schema({
   from_email: {type: String, required: true},
   to_email: {type: String, required: true},
-  body: {type: String, required: true}
+  body: {type: String, required: true},
+  sent_messages: {type: Array}
 })
 
-const Message = mongoose.model('Message', MessageSchema);
+const Conversation = mongoose.model('Conversation', ConversationSchema);
 
-module.exports = Message;
+module.exports = Conversation;
