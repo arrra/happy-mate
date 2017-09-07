c62629ba02cb9589058f3d931c3bc1d37eeea8d4
Sagar Singh <ssing128@gmail.com>
2017-09-06 19:52:23 -0700

    update conversation schema

--- models/Conversation.js
+++ models/Conversation.js
@@ -4,9 +4,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ConversationSchema = new Schema({
-  from_email: {type: String, required: true},
-  to_email: {type: String, required: true},
-  body: {type: String, required: true},
+  from_email: {type: String, required: true, unique: true},
+  to_email: {type: String, required: true, unique: true},
   sent_messages: {type: Array}
 })
 
