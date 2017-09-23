ba703a3bcbf23b29d7a722a2c3b3cc7491e475dd
Sagar Singh <ssing128@gmail.com>
2017-09-22 20:04:54 -0700

    generate random message

--- models/Message.js
+++ models/Message.js
@@ -2,10 +2,17 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const util = require('../util/index');
 
 const MessageSchema = new Schema({
   body: {type: String, required: true},
 })
+
+MessageSchema.statics.getRandomMessage = function(cb) {
+  return this.find({},function(err, messages) {
+    cb(err, messages[util.getRandomNumber(0, messages.length)])
+  });
+}
 
 const Message = mongoose.model('Message', MessageSchema);
 
