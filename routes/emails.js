4f69831d1e7e5254b81b294f7fd56177028ff32f
Sagar Singh <ssing128@gmail.com>
2017-09-25 21:46:32 -0700

    unnecessary to return err

--- routes/emails.js
+++ routes/emails.js
@@ -10,7 +10,7 @@ router.post('/', (req, res) => {
   message.save((err, message) => {
     if (err){
       res.status(400).json(err);
-      return err;
+      return;
     }
 
     res.status(201).json(message);
