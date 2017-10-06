6a1279fb0b43da170ae3c80a995fd7951200331c
Sagar Singh <ssing128@gmail.com>
2017-10-06 08:33:57 -0700

    create GET /confirmation route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const emails = require('./emails');
 const conversations = require('./conversations');
+const confirmation = require('./confirmation');
 
 const attachRoutes = (app) => {
   app.use('/emails', emails);
   app.use('/conversations', conversations);
+  app.use('/confirmation', confirmation);
 };
 
 module.exports = {
