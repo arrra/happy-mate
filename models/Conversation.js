529305fc23d06c271dd11eb02651e1fb0da5054f
Sagar Singh <ssing128@gmail.com>
2017-10-08 16:15:35 -0700

    Update conversation

--- models/Conversation.js
+++ models/Conversation.js
@@ -9,6 +9,7 @@ const ConversationSchema = new Schema({
   to_email: { type: String, required: true },
   sent_messages: { type: Array },
   messagePool: { type: Array },
+  emailVerified: { type: Boolean, default: false },
 });
 
 ConversationSchema.method({
