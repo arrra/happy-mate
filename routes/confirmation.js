0dcae03b00df09e950a09b602eb8fc2c43c52d9d
Sagar Singh <ssing128@gmail.com>
2017-10-09 21:17:15 -0700

    Server able to send confirmation link

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -14,4 +14,31 @@ router.get('/:id', (req, res) => {
   });
 });
 
+router.put('/:id/send-confirmation', (req, res) => {
+  Conversation.findById(req.params.id, (err, conversation) => {
+    if (err) {
+      res.status(404).json(err);
+      return;
+    }
+    const subject = 'Verify your email';
+    const from = 'ssing128@gmail.com';
+    const body = 'http://localhost:8080/#/confirmation/'+conversation._id;
+    const mail = new Mail(
+      conversation.to_email,
+      from,
+      subject,
+      body,
+      process.env.CONFIRMATION_EMAIL_TEMPLATE_ID,
+    );
+
+    mail.sendEmail((err) => {
+      if (err) {
+        res.status(500).json(err);
+        return;
+      }
+      res.status(200).end();
+    });
+  });
+});
+
 module.exports = router;
