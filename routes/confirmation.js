a19ec285422f56b0fbc8fac528eea353b172c3bc
Sagar Singh <ssing128@gmail.com>
2017-10-09 21:15:22 -0700

    change put to get

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -1,9 +1,10 @@
 const Router = require('express').Router;
 const Conversation = require('../models/Conversation');
+const Mail = require('../classes/Mail');
 
 const router = Router();
 
-router.put('/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   Conversation.findByIdAndUpdate(req.params.id, {emailVerified: true}, { new: true }, (err, conversation) => {
     if(err){
       res.status(500).json(err);
