fa2a29790e6d96a348caf93b3e8dd4617d09136c
Sagar Singh <ssing128@gmail.com>
2017-10-11 00:43:45 -0700

    unwanted code

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const emails = require('./emails');
 const conversations = require('./conversations');
-const confirmation = require('./confirmation');
 
 const attachRoutes = (app) => {
   app.use('/emails', emails);
   app.use('/conversations', conversations);
-  app.use('/confirmation', confirmation);
 };
 
 module.exports = {
