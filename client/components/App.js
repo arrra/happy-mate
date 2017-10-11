ea71b8ce3dd8474e55763e894026fb2989e1f83b
Sagar Singh <ssing128@gmail.com>
2017-10-11 00:53:13 -0700

    Render component on exact conversation/:id

--- client/components/App.js
+++ client/components/App.js
@@ -18,6 +18,7 @@ class App extends React.Component {
           <Navbar />
           <Route exact path="/" component={SendMessageForm} />
           <Route
+            exact
             path="/conversations/:id"
             render={props => (<ConversationPage
               conversationId={props.match.params.id}
