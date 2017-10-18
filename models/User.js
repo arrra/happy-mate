04066e1df1cf1435addae1ec5479158a5ddeecb7
Sagar Singh <ssing128@gmail.com>
2017-10-18 16:26:54 -0700

    Hash password inside createUser

--- models/User.js
+++ models/User.js
@@ -20,21 +20,6 @@ UserSchema.set('toJSON', {
   },
 });
 
-UserSchema.pre('save', function (next) {
-  const user = this;
-
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    if (err) return err;
-
-    bcrypt.hash(user.passwordHash, salt, (err, hash) => {
-      if (err) return err;
-
-      user.passwordHash = hash;
-      next(err);
-    });
-  });
-});
-
 UserSchema.methods.comparePassword = function (password, cb) {
   bcrypt.compare(password, this.passwordHash, (err, isMatch) => {
     if (err) return cb(err);
@@ -61,13 +46,23 @@ UserSchema.statics.authenticate = function (userName, password, cb) {
 };
 
 UserSchema.statics.createUser = function (userName, password, cb) {
-  const user = new User({ userName, passwordHash: password });
 
-  user.save((err) => {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, this);
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    if (err) return err;
+
+    bcrypt.hash(password, salt, (err, hash) => {
+      if (err) return err;
+
+      const user = new User({ userName, passwordHash: hash });
+
+      user.save((err) => {
+        if (err) {
+          return cb(err);
+        }
+
+        cb(null, this);
+      });
+    });
   });
 };
 
