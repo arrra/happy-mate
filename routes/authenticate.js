46c4d92be7792a34884f57dbf0a8ec854585d62c
Sagar Singh <ssing128@gmail.com>
2017-10-18 18:28:02 -0700

    Seperate getting a token logic

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -4,6 +4,18 @@ const jwt = require('jsonwebtoken');
 
 const router = Router();
 
+const getAuthToken = (user) => {
+  const payload = {
+    iss: 'localhost:3000',
+    name: user.userName,
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, process.env.SECRET);
+
+  return token;
+}
+
 router.post('/', (req, res) => {
   User.authenticate(req.body.userName, req.body.password, (err, user) => {
     if (err) {
@@ -11,15 +23,7 @@ router.post('/', (req, res) => {
       return;
     }
 
-    const payload = {
-      iss: 'localhost:3000',
-      name: user.userName,
-      id: user._id,
-    };
-
-    const token = jwt.sign(payload, process.env.SECRET);
-
-    res.cookie('token', token);
+    res.cookie('token', getAuthToken(user));
     res.status(200).json(user);
   });
 });
