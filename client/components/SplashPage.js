1d4ae3466cb28e21b782ceeac5bc5d5f52469d56
Sagar Singh <ssing128@gmail.com>
2017-10-20 01:51:12 -0700

    Set / route to SplashPage component

---
+++ client/components/SplashPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import SignUp from './SignUp';
+
+class SplashPage extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="main">
+          <div className="row">
+            <div className="eight columns">
+              <div className="title">
+                <h2>"I love you" emails - automated</h2>
+              </div>
+              <div className="title-sub">
+                <h6>Now you dont have to take out time to send a cute email to your lover! We Automated it!</h6>
+              </div>
+            </div>
+            <div className="one columns">
+              <div className="divider">
+              </div>
+            </div>
+            <div className="three columns">
+              <SignUp />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SplashPage;
